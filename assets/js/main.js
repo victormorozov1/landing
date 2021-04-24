8ac1438cde97889721eac667800a243648c28806
victormorozov1 <vismo@bk.ru>
2021-04-24 18:35:34 +0500

    add base

---
+++ assets/js/main.js
@@ -0,0 +1,12 @@
+$(document).ready(function(){
+    var image = document.getElementsByClassName('thumbnail');
+    new simpleParallax(image);
+
+    // $("nav").mouseover(function (event){
+    //     $("#main-section").css('background-color', '#000000');
+    // });
+    //
+    // $("nav").mouseout(function (event){
+    //     $("#main-section").css('background-color', '#555555');
+    // });
+});
