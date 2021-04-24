9acb31e4ce61ea1fce4a497c7d994304ead9be5a
victormorozov1 <vismo@bk.ru>
2021-04-25 04:55:32 +0500

    add statistics

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,6 +1,20 @@
 $(document).ready(function(){
+    function set_info(id, cost, people, mark){
+        $(statistics_node).children("#cost").children("p").text(cost + " млн. $");
+        $(statistics_node).children("#people").children("p").text(people + " тыс.");
+        $(statistics_node).children("#mark").children("p").text(mark + " средняя оценка");
+        $("#" + id).append(statistics_node.clone());
+    }
+
     var image = document.getElementsByClassName('thumbnail');
     new simpleParallax(image);
+    let statistics_node = $('.statistics').clone();
+
+    set_info("iceberg", "280", "12", "4.8")
+    set_info("adler", "328", "8", "4.7")
+    set_info("puck", "35", "7", "4.8")
+    set_info("grand-ice-palace", "300", "12", "4.8")
+    set_info("curling-center", "30", "3", "4.7")
 
     $(".menu-icon").click(function (){
         $(this).css("display", "none");
@@ -16,4 +30,6 @@ $(document).ready(function(){
             $(".menu-icon").css("display", "block");
         }
     });
+
+
 });
